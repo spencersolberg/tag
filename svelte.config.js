7e0addc13d51c6fa523803f5a441bf121fafeab9
Spencer <you@example.com>
2021-09-05 15:31:09 -0400

    fixed adapter & about route

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
 import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-        adapater: require("@sveltejs/adapter-static")(),
+        adapter: adapter(),
 		target: '#svelte'
 	},
 
