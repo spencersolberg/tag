e4ff1791c40a0488dbe57cca9b42a920f61d68e3
Spencer <you@example.com>
2021-09-13 20:18:36 -0400

    About page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,3 +6,7 @@
     <meta name="twitter:description" content="Barbershop Tag Platform" />
 </svelte:head>
 <h1 class="text-4xl mb-8 text-center uppercase text-primary-black dark:text-primary-white font-medium">About</h1>
+
+<p class=" text-lg text-primary-black text-center dark:text-primary-white">Made by Spencer Solberg</p>
+<p class="underline text-lg text-primary-red text-center dark:text-primary-red"><a href="https://twitter.com/spencersolberg_">Twitter</a></p>
+<p class="underline text-lg text-primary-red text-center dark:text-primary-red"><a href="mailto:spencer@tags.town">eMail</a></p>
